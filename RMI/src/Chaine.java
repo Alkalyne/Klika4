8e14dc532563569559b7b5816fc923e72ca41ce3
P0ppoff <jules.hablot@gmail.com>
2016-02-21 11:10:43 +0100

    Class Chaine ready to test

--- RMI/src/Chaine.java
+++ RMI/src/Chaine.java
@@ -1,13 +1,28 @@
 package src;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Chaine implements _Chaine {
-
+	
+	private List<Hotel> possession = new LinkedList<Hotel>();
+	
+	public Chaine (List<Hotel> l) {
+		this.possession = l;
+	}
+	
 	@Override
 	public List<Hotel> get(String localisation) {
-		// TODO Auto-generated method stub
-		return null;
+		Iterator<Hotel> i = possession.iterator();
+		List<Hotel> region = new LinkedList<Hotel>();
+		while(i.hasNext()){
+			Hotel hCourant = i.next();
+			if(hCourant.localisation == localisation){
+				region.add(hCourant);
+			};	
+		}
+		return region;
 	}
 
 }
