b8dde74dbf176f2cee876936f4cb1bb95f691a3e
P0ppoff <jules.hablot@gmail.com>
2016-02-29 11:17:00 +0100

    add client implementation RMI

--- RMI/src/Client/Client.java
+++ RMI/src/Client/Client.java
@@ -5,9 +5,27 @@ public class Client {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String args[]) {
+		String nom="", host="localhost", localisation="Paris";
+		// récupération des arguments
+		if (args.length!=2){
+			System.out.println("Client <hostname> <nom générique des objets distants> <localisation>");
+			System.exit(1);
+		}
+		try {
+			host = args[0];
+			nom = args[1];
+			localisation = args[2];
+		}catch(Exception e) {
+			System.out.println("Client <hostname> <nom générique des objets distants> <localisation>");
+			System.exit(1);
+		}	
+		// installation d'un securityManager 
+		// A COMPLETER : INSTALLATION DU SECURITYMANAGER
+		/*if(System.getSecurityManager() == null){
+			System.setSecurityManager(new SecurityManager());
+		}*/
+		// Démarrage des consommateurs
+		new LookForHotel(host, nom, localisation).call();
 	}
-
 }
--- RMI/src/Client/LookForHotel.java
+++ RMI/src/Client/LookForHotel.java
@@ -4,6 +4,14 @@
 
 package src.Client;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Random;
+
+import src.commun._Chaine;
+
 /**
  * Représente un client effectuant une requête lui permettant d'obtenir les numéros de téléphone des hôtels répondant à son critère de choix.
  * @author  Morat
@@ -11,14 +19,20 @@ package src.Client;
 public class LookForHotel{
 	/** le critère de localisaton choisi */
 	private String localisation;
-	// ...
+	private String ou;
+	private String qui;
+	
+	private _Chaine obj;
+	
 	/**
 	 * Définition de l'objet représentant l'interrogation.
 	 * @param args les arguments n'en comportant qu'un seul qui indique le critère
 	 *          de localisation
 	 */
 	public LookForHotel(String... args){
-		localisation = args[0];
+		ou = args[0];
+		qui = args[1];
+		localisation = args[2];
 	}
 	/**
 	 * réalise une intérrogation
@@ -26,8 +40,22 @@ public class LookForHotel{
 	 * @throws RemoteException
 	 */
 	public long call() {
-		// ...
+		try {
+			String name = "//" + ou + "/" + qui;
+			System.out.println(this + " -> " + name);
+			obj = (_Chaine) Naming.lookup(name);
+			System.out.println("Liste des hotels à " + localisation + " : " + obj.get(localisation));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
 	}
 
-	// ...
 }
