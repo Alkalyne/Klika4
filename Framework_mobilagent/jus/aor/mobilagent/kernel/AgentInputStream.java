998a8c75bfc043083096db692ea7e6a615cc50f5
P0ppoff <jules.hablot@gmail.com>
2016-03-31 15:02:32 +0200

    more readable AgentInputStream

--- Framework_mobilagent/jus/aor/mobilagent/kernel/AgentInputStream.java
+++ Framework_mobilagent/jus/aor/mobilagent/kernel/AgentInputStream.java
@@ -14,6 +14,13 @@ class AgentInputStream extends ObjectInputStream{
      * le classLoader à utiliser 
      */
     BAMAgentClassLoader loader;
-    AgentInputStream(InputStream is, BAMAgentClassLoader cl) throws IOException{super(is); loader = cl;}
-    protected Class<?> resolveClass(ObjectStreamClass cl) throws IOException,ClassNotFoundException{return loader.loadClass(cl.getName());}
+    AgentInputStream(InputStream is, BAMAgentClassLoader cl) throws IOException{
+    	super(is); 
+    	loader = cl;
+    }
+    
+    protected Class<?> resolveClass(ObjectStreamClass cl) 
+    		throws IOException,ClassNotFoundException{
+    	return loader.loadClass(cl.getName());
+    }
 }
