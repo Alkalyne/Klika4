d935fd995c675c4c8e9e0d1898cfedfb90d58bfc
camboui <mathias.branger@gmail.com>
2016-04-01 07:59:57 +0200

    ajout serviceDescriptor

---
+++ Framework_mobilagent/jus/aor/mobilagent/kernel/ServiceDescriptor.java
@@ -0,0 +1,24 @@
+package jus.aor.mobilagent.kernel;
+
+public class ServiceDescriptor {
+
+	public ServiceDescriptor(){
+		
+	}
+	
+	public void name(){
+		
+	}
+	
+	public void haschCode(){
+		
+	}
+	
+	public void equals(){
+		
+	}
+	
+	public String toString(){
+		return null;
+	}
+}
